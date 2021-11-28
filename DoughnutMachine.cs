7e253c6c613a62beb121b566cda67de9b3a145e7
Deak Levente <Dlevi93@outlook.com>
2021-11-28 14:38:42 +0200

    Add project files.

---
+++ DoughnutMachine.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Threading;
+
+namespace Deak_Levente_Ferenc_Lab3
+{
+    public class DoughnutMachine : Component
+    {
+        private DoughnutType mFlavor;
+        public DoughnutType Flavor { get => mFlavor; set => mFlavor = value; }
+
+        private System.Collections.ArrayList mDoughnuts = new System.Collections.ArrayList();
+        public Doughnut this[int Index] { get => (Doughnut)mDoughnuts[Index]; set => mDoughnuts[Index] = value; }
+
+        public bool Enabled { set => doughnutTimer.IsEnabled = value; }
+        public int Interval { set => doughnutTimer.Interval = new TimeSpan(0, 0, value); }
+
+        public delegate void DoughnutCompleteDelegate();
+        public event DoughnutCompleteDelegate DoughnutComplete;
+
+        DispatcherTimer doughnutTimer;
+
+        public DoughnutMachine()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            doughnutTimer = new DispatcherTimer();
+            doughnutTimer.Tick += doughnutTimer_Tick;
+        }
+
+        private void doughnutTimer_Tick(object sender, EventArgs e)
+        {
+            Doughnut aDoughnut = new Doughnut(this.Flavor);
+            mDoughnuts.Add(aDoughnut);
+            DoughnutComplete();
+        }
+
+        public void MakeDoughnuts(DoughnutType dFlavor)
+        {
+
+            Flavor = dFlavor;
+            switch (Flavor)
+            {
+                case DoughnutType.Glazed: Interval = 3; break;
+                case DoughnutType.Sugar: Interval = 2; break;
+                case DoughnutType.Lemon: Interval = 5; break;
+                case DoughnutType.Chocolate: Interval = 7; break;
+                case DoughnutType.Vanilla: Interval = 4; break;
+            }
+            doughnutTimer.Start();
+        }
+    }
+
+    public class Doughnut
+    {
+        private DoughnutType mFlavor;
+
+        public DoughnutType Flavor { get => mFlavor; set => mFlavor = value; }
+        private float mPrice = .50F;
+        public float Price { get => mPrice; set => mPrice = value; }
+        private readonly DateTime mTimeOfCreation;
+        public DateTime TimeOfCreation => mTimeOfCreation;
+
+        public Doughnut(DoughnutType aFlavor) // constructor
+        {
+            mTimeOfCreation = DateTime.Now;
+            mFlavor = aFlavor;
+        }
+    }
+
+    public enum DoughnutType
+    {
+        Glazed,
+        Sugar,
+        Lemon,
+        Chocolate,
+        Vanilla
+    }
+}
---
+++ StopCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+namespace Deak_Levente_Ferenc_Lab4.CustomCommands
+{
+    public class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch",
+                typeof(StopCommand), myInputGestures);
+        }
+        public static RoutedUICommand Launch => launch_command;
+    }
+}
